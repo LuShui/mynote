052552f33d2d7e12be1ebd2e5ca35a1d97d62799
Lushui <718093910@qq.com>
2018-01-19 00:06:19 +0800

    note model

--- Apps/Web/Controller/IndexController.class.php
+++ Apps/Web/Controller/IndexController.class.php
@@ -3,7 +3,39 @@ namespace Web\Controller;
 use Think\Controller;
 class IndexController extends Controller {
 
+	//显示首页模板
     public function index(){
     	$this->display();
+    }
+
+    //获取首页数据
+    public function homesoure(){
+    	$page = I('post.pageindex');
+        $pagecount = 15;
+        $pageindex = $page * $pagecount;
+
+        $sql = "SELECT notetable.*,typetable.title as typename FROM notetable LEFT JOIN  typetable ON notetable.type=typetable.id where ishow=1 ORDER BY addtime desc LIMIT %d,$pagecount;";
+    	$list = M()->query($sql,$pageindex);
+
+    	if ($list) {
+    		$backarr = array(
+    			'code'=>1,
+    			'ctn'=>$list
+    		);
+    	}else{
+    		$backarr = array(
+    			'code'=>0
+    		);
+    	}
+    	echo json_encode($backarr);
+    }
+
+
+    public function detil(){
+        $id = I('get.id');
+        $json = M('notetable')->where("id=$id")->find();
+        $this->assign('json',$json);
+        $this->assign('html',htmlspecialchars_decode($json['ctn']));
+        $this->display();
     }
 }
--- Runtime/Cache/Web/d08529be7684aba9e04471e265f9a446.php
+++ Runtime/Cache/Web/d08529be7684aba9e04471e265f9a446.php
@@ -12,7 +12,9 @@
     <link rel="stylesheet" href="/mynote/Public/css/bootstrap.min.css">
     <link rel="stylesheet" href="/mynote/Public/css/web.css">
     <link rel="stylesheet" href="/mynote/Public/layer/css/layui.css">
-
+    <link rel="stylesheet" href="/mynote/Public/loading/jquery.mloading.css">
+    <script type="text/javascript" src="/mynote/Public/js/jquery-2.1.4.min.js"></script>
+    <script src="/mynote/Public/loading/jquery.mloading.js"></script>
 </head>
 <body>
     <div id="warp" class="container">
@@ -29,11 +31,12 @@
                 </div>
             </header>
         </div>
-
+    </div>
+    <div class="container">
         <div class="contentbox row">
             <div class="session col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                <ul class="contentlist">
-                    <li class="artleitem">
+                <ul class="contentlist" id="contentlist">
+                    <!-- <li class="artleitem">
                         <a class="artlebox">
                             <div class="artle-title-box">
                                 <div class="artle-category">JavaScript</div>
@@ -54,139 +57,7 @@
                                     点击量(5000)</span>
                             </div>
                         </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="artleitem">
-                        <a class="artlebox">
-                            <div class="artle-title-box">
-                                <div class="artle-category">JavaScript</div>
-                                <div class="artle-titlename">这是JavaScript文章，写的非常好</div>
-                            </div>
-                            <div class="artil-content-box">
-                                上套课程讲了Koa2，挑战全栈就必须和数据库打交道，个人觉的作为一个前端开发人员，操作MongoDB还是比较惬意的。因为它是一种NoSql数据库，不用写SQL语句，而且里边的用法都是JSON对象的形式。所以我们在Koa2的课程中间查了Mon...
-                            </div>
-                            <div class="artil-info-box">
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>
-                                    2018-01-3</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>
-                                    隔壁老王</span>
-                                <span class="artle-time-span">
-                                    <i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>
-                                    点击量(5000)</span>
-                            </div>
-                        </a>
-                    </li>
+                    </li> -->
                 </ul>
             </div>
             <div class="adbox col-sm-3 col-md-3 col-lg-3">
@@ -214,5 +85,81 @@
         </div>
     </div>
 </body>
-<script type="text/javascript" src="/mynote/Public/js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript">
+
+$(function(){ 
+    var pageindex = 0; 
+    $(window).bind('scroll',function show()  {  
+        if($(window).scrollTop()+$(window).height()>=$(document).height())  { 
+            pageindex ++; 
+            getSoure(pageindex);  
+        }  
+    }); 
+
+    function getSoure(pageindex){
+        $("body").mLoading('show');
+        $.ajax({  
+            type:'post',  
+            dataType:'json',  
+            url:"<?php echo U('index.php/Web/index/homesoure');?>",  
+            success:function(res){ 
+                if (res.code == 1) {
+                    htmlrender(res.ctn);
+                }
+                $('body').mLoading("hide"); 
+            } ,
+            fail:function(){
+                $('body').mLoading("hide");
+            } 
+        });
+    };
+
+    function htmlrender(array){
+        let html = '';
+        var eles = document.createElement('div');
+        for (var i = 0; i < array.length; i++) {
+            var json = array[i];
+            eles.innerHTML = json.ctn;
+            var temp = filterHTMLTag($(eles).text());//.replace(/<[^>]+>/g,"");
+
+            html += '<li class="artleitem">'+
+                        '<a class="artlebox" onClick="gotodetil('+json.id+')">'+
+                            '<div class="artle-title-box">'+
+                                '<div class="artle-category">'+json.typename+'</div>'+
+                                '<div class="artle-titlename">'+json.title+'</div>'+
+                            '</div>'+
+                            '<div class="artil-content-box">'+ json.des + temp +'</div>'+
+                            '<div class="artil-info-box">'+
+                                '<span class="artle-time-span">'+
+                                    '<i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe637;</i>2018-01-3</span>'+
+                                '<span class="artle-time-span">'+
+                                    '<i class="layui-icon artle-icon" style="font-size: 13px; color: #999999;">&#xe612;</i>隔壁老王</span>'+
+                                '<span class="artle-time-span">'+
+                                    '<i class="layui-icon artle-icon" style="font-size: 13px; color: #666666;">&#xe705;</i>点击量('+json.num+')</span>'+
+                            '</div>'+
+                        '</a>'+
+                    '</li>'
+        }
+        $('#contentlist').append(html);
+    } 
+
+    var filterHTMLTag = function (msg) {
+        var msg = msg.replace(/<\/?[^>]*>/g, ''); //去除HTML Tag
+        msg = msg.replace(/[|]*\n/, '') //去除行尾空格
+        msg = msg.replace(/&npsp;/ig, ''); //去掉npsp
+        return msg;
+    }
+    getSoure(pageindex); 
+});
+
+    function gotodetil(id){
+        var url = "<?php echo U('index.php/Web/index/detil');?>" + '/id/' +id;
+        console.log(url);
+        window.location.href= url;
+        // $.post("<?php echo U('index.php/Web/index/detil');?>",{'id':id},function(result){
+        //     window.loction.href="U('index.php/Web/index/detil"
+        // });        
+    }
+
+</script>
 </html>
